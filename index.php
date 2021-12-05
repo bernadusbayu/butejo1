017acd4f9e1198fff17684bcb4993ba45a69e64d
bernadusbayu <75233557+bernadusbayu@users.noreply.github.com>
2021-12-05 19:22:43 +0700

    update index

--- index.php
+++ index.php
@@ -7,11 +7,13 @@ $is_production = false;
 $api_url = $is_production ? 
   'https://app.midtrans.com/snap/v1/transactions' : 
   'https://app.sandbox.midtrans.com/snap/v1/transactions';
-  
+
+
 if( !strpos($_SERVER['REQUEST_URI'], '/charge') ) {
-http_response_code(404); 
-echo "wrong path, make sure it's `/charge`"; exit();
+  http_response_code(404); 
+  echo "wrong path, make sure it's `/charge`"; exit();
 }
+
 if( $_SERVER['REQUEST_METHOD'] !== 'POST'){
   http_response_code(404);
   echo "Page not found or wrong HTTP request method is used"; exit();
@@ -25,6 +27,7 @@ $charge_result = chargeAPI($api_url, $server_key, $request_body);
 http_response_code($charge_result['http_code']);
 
 echo $charge_result['body'];
+
 
 function chargeAPI($api_url, $server_key, $request_body){
   $ch = curl_init();
@@ -42,7 +45,10 @@ function chargeAPI($api_url, $server_key, $request_body){
     ),
     CURLOPT_POSTFIELDS => $request_body
   );
-  
-
-
-echo $charge_result['body'];
+  curl_setopt_array($ch, $curl_options);
+  $result = array(
+    'body' => curl_exec($ch),
+    'http_code' => curl_getinfo($ch, CURLINFO_HTTP_CODE),
+  );
+  return $result;
+}
